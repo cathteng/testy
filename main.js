7cfe8439cf97260b0d50bbd0a7c98ccb1a60d7fc
Cathy Teng <cathy.teng@sentry.io>
2023-11-28 14:20:03 -0800

    another error

--- main.js
+++ main.js
@@ -15,7 +15,7 @@ const transaction = Sentry.startTransaction({
   
   setTimeout(() => {
     try {
-      hello();
+      wowee();
     } catch (e) {
       Sentry.captureException(e);
     } finally {
