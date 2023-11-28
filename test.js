60f923803040fcb3316edabaa3e05cecd7466fb7
Cathy Teng <cathy.teng@sentry.io>
2023-11-28 14:21:42 -0800

    new file

---
+++ test.js
@@ -0,0 +1,24 @@
+const Sentry = require("@sentry/node");
+
+Sentry.init({
+  dsn: 'https://b070dad593840b171d307dbb38de729f@o4505365730885632.ingest.sentry.io/4506305809088512',
+  // Performance Monitoring
+  tracesSampleRate: 1.0,
+  // Set sampling rate for profiling - this is relative to tracesSampleRate
+  profilesSampleRate: 1.0,
+});
+
+const transaction = Sentry.startTransaction({
+    op: "test",
+    name: "My First Test Transaction",
+  });
+  
+  setTimeout(() => {
+    try {
+      testy();
+    } catch (e) {
+      Sentry.captureException(e);
+    } finally {
+      transaction.finish();
+    }
+  }, 99);
